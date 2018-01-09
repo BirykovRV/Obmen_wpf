52f53062cfa252fe0e7f14be1c17aba8d9bcd4dd
Roman Birukov <r.v.birukov@yandex.ru>
2018-01-09 16:56:23 +0300

    настройки

--- Obmen_wpf/Model/GetSettings.cs
+++ Obmen_wpf/Model/GetSettings.cs
@@ -1,23 +1,13 @@
-﻿using System.Xml.Linq;
+﻿using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
 
 namespace Obmen_wpf.Model
 {
     class GetSettings
     {
-        public void CreateXml()
+        public void jsonSerializer()
         {
-            XDocument xdoc = new XDocument(new XElement("Config",
-                new XElement("F130",
-                    new XAttribute("name", "f130"),
-                    new XElement("from", "\\F130"),
-                    new XElement("to", "D:\\TEST\\")),
-
-                new XElement("PostPay",
-                    new XAttribute("name", "postpay"),
-                    new XElement("from", "\\PostPay"),
-                    new XElement("to", "D:\\TEST\\"))));
-
-            xdoc.Save("D:\\TEST\\config.xml");
+            
         }
     }
 }
