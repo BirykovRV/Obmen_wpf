dcc3666aa4e10c3f0c7a4400e0a39fa317ba2e6b
Roman Birukov <r.v.birukov@yandex.ru>
2018-01-22 16:21:26 +0300

    добавлены команды для всех кнопок

--- Obmen_wpf/ViewModel/UserControlViewModel.cs
+++ Obmen_wpf/ViewModel/UserControlViewModel.cs
@@ -23,12 +23,31 @@ namespace Obmen_wpf.ViewModel
                     switch (o.ToString())
                     {
                         case "asku":
-                            FolderBrowserDialog dialog = new FolderBrowserDialog();
-                            dialog.ShowDialog();
-                            Properties.Settings.Default.f130From = dialog.SelectedPath;
+                            DoOperation(TypeOfOperation.ASKU);
                             break;
                         case "config":
-                            MessageBox.Show("config");
+                            DoOperation(TypeOfOperation.CONFIG);
+                            break;
+                        case "post_update":
+                            DoOperation(TypeOfOperation.POSTPAY_UPDATE);
+                            break;
+                        case "post_db":
+                            DoOperation(TypeOfOperation.POSTPAY_DB);
+                            break;
+                        case "post_reg":
+                            DoOperation(TypeOfOperation.POSTPAY_REG);
+                            break;
+                        case "espp":
+                            DoOperation(TypeOfOperation.ESPP);
+                            break;
+                        case "pension":
+                            DoOperation(TypeOfOperation.PENSION);
+                            break;
+                        case "fsg_reg":
+                            DoOperation(TypeOfOperation.FSG_REG);
+                            break;
+                        case "fsg_cash":
+                            DoOperation(TypeOfOperation.FSG_CASH);
                             break;
 
                     }
@@ -41,5 +60,56 @@ namespace Obmen_wpf.ViewModel
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(sender));
         }
+
+        private void DoOperation(TypeOfOperation type)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            switch (type)
+            {
+                case TypeOfOperation.ASKU:
+                    Properties.Settings.Default.f130From = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.CONFIG:
+                    Properties.Settings.Default.configTo = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.POSTPAY_UPDATE:
+                    Properties.Settings.Default.postPayUpdateTo = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.POSTPAY_DB:
+                    Properties.Settings.Default.postPayDBTo = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.POSTPAY_REG:
+                    Properties.Settings.Default.postPayRegFrom = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.ESPP:
+                    Properties.Settings.Default.esppTo = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.PENSION:
+                    Properties.Settings.Default.pensionFrom = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.FSG_REG:
+                    Properties.Settings.Default.fsgRegFrom = dialog.SelectedPath;
+                    break;
+                case TypeOfOperation.FSG_CASH:
+                    Properties.Settings.Default.cashFsgTo = dialog.SelectedPath;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+}
+
+enum TypeOfOperation
+{
+    ASKU,
+    CONFIG,
+    POSTPAY_UPDATE,
+    POSTPAY_DB,
+    POSTPAY_REG,
+    ESPP,
+    PENSION,
+    FSG_REG,
+    FSG_CASH
 }
