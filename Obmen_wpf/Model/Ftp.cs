99f32d55b04c1596879aa8bf90c67db8976b7a80
Roman Birukov <r.v.birukov@yandex.ru>
2018-02-06 16:51:24 +0300

    Создание папки на сервере

--- Obmen_wpf/Model/Ftp.cs
+++ Obmen_wpf/Model/Ftp.cs
@@ -22,59 +22,66 @@ namespace Obmen_wpf.Model
         /// <param name="localPath">Путь куда скачивать файлы на локальном компьютере</param>
         public void Download(string remotePath, string localPath)
         {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Url + remotePath);
-            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
-            request.Credentials = new NetworkCredential(Username, Password);
-
-            List<string> lines = new List<string>();
-
-            using (FtpWebResponse listResponse = (FtpWebResponse)request.GetResponse())
-            using (Stream listStream = listResponse.GetResponseStream())
-            using (StreamReader listReader = new StreamReader(listStream))
+            try
             {
-                while (!listReader.EndOfStream)
-                {
-                    lines.Add(listReader.ReadLine());
-                }
-            }
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Url + remotePath);
+                request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                request.Credentials = new NetworkCredential(Username, Password);
 
-            foreach (var line in lines)
-            {
-                string[] tokens = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                // имя файла
-                string name = tokens[8];
-                // Тип файла ('d' - папка)
-                string category = tokens[0];
-                string combinedLocalPath = Path.Combine(localPath, name);
-                string fileUrl = remotePath + name;
+                List<string> lines = new List<string>();
 
-                if (category.StartsWith("d"))
+                using (FtpWebResponse listResponse = (FtpWebResponse)request.GetResponse())
+                using (Stream listStream = listResponse.GetResponseStream())
+                using (StreamReader listReader = new StreamReader(listStream))
                 {
-                    if (!Directory.Exists(combinedLocalPath))
+                    while (!listReader.EndOfStream)
                     {
-                        Directory.CreateDirectory(combinedLocalPath);
+                        lines.Add(listReader.ReadLine());
                     }
-
-                    Download(fileUrl + "/", combinedLocalPath);
                 }
-                else
-                {
-                    FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(Url + fileUrl);
-                    downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
-                    downloadRequest.Credentials = new NetworkCredential(Username, Password);
 
-                    using (FtpWebResponse response = (FtpWebResponse)downloadRequest.GetResponse())
-                    using (Stream responseStream = response.GetResponseStream())
-                    using (Stream targetStream = File.Create(combinedLocalPath))
+                foreach (var line in lines)
+                {
+                    string[] tokens = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    // имя файла
+                    string name = tokens[8];
+                    // Тип файла ('d' - папка)
+                    string category = tokens[0];
+                    string combinedLocalPath = Path.Combine(localPath, name);
+                    string fileUrl = remotePath + name;
+
+                    if (category.StartsWith("d"))
                     {
-                        byte[] buffer = new byte[256];
-                        int bytesRead;
-                        while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                        if (!Directory.Exists(combinedLocalPath))
                         {
-                            targetStream.Write(buffer, 0, bytesRead);
+                            Directory.CreateDirectory(combinedLocalPath);
+                        }
+
+                        Download(fileUrl + "/", combinedLocalPath);
+                    }
+                    else
+                    {
+                        FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(Url + fileUrl);
+                        downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                        downloadRequest.Credentials = new NetworkCredential(Username, Password);
+
+                        using (FtpWebResponse response = (FtpWebResponse)downloadRequest.GetResponse())
+                        using (Stream responseStream = response.GetResponseStream())
+                        using (Stream targetStream = File.Create(combinedLocalPath))
+                        {
+                            byte[] buffer = new byte[256];
+                            int bytesRead;
+                            while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                targetStream.Write(buffer, 0, bytesRead);
+                            }
                         }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
         }
         /// <summary>
@@ -84,16 +91,43 @@ namespace Obmen_wpf.Model
         /// <param name="remotePath">Путь к папке на сервере с именем файла</param>
         public void Upload(string localPath, string remotePath)
         {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Url + remotePath);
-            request.Method = WebRequestMethods.Ftp.UploadFile;
-            request.Credentials = new NetworkCredential(Username, Password);
-                        
-            using (FileStream uploadedFile = new FileStream(localPath, FileMode.Open, FileAccess.Read))
+            try
             {
-                byte[] byteBuffer = new byte[uploadedFile.Length];
-                uploadedFile.Read(byteBuffer, 0, byteBuffer.Length);
-                using (Stream writer = request.GetRequestStream())
-                    writer.Write(byteBuffer, 0, byteBuffer.Length);
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Url + remotePath);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential(Username, Password);
+
+                using (FileStream uploadedFile = new FileStream(localPath, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] byteBuffer = new byte[uploadedFile.Length];
+                    uploadedFile.Read(byteBuffer, 0, byteBuffer.Length);
+                    using (Stream writer = request.GetRequestStream())
+                        writer.Write(byteBuffer, 0, byteBuffer.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Создание папки на сервере
+        /// </summary>
+        /// <param name="path">Путь где создать папку</param>
+        public void CreateDir(string path)
+        {
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Url + path);
+                request.Method = WebRequestMethods.Ftp.MakeDirectory;
+                request.Credentials = new NetworkCredential(Username, Password);
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
         }
     }
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -22,18 +22,24 @@ namespace Testing
 
             string localPath = "D:\\TEST\\";
             string remotePath = "test/";
+            ftp.CreateDir(remotePath + "1");
+            //DirectoryInfo info = new DirectoryInfo(localPath);
+            //FileInfo [] files = info.GetFiles();
+            //DirectoryInfo[] dirs = info.GetDirectories(localPath);
 
-            DirectoryInfo info = new DirectoryInfo(localPath);
-            FileInfo [] fileInfo = info.GetFiles();
 
-            foreach (var file in fileInfo)
-            {
-                Task.Factory.StartNew(()=>
-                {
-                    Console.WriteLine($"Копирование:\t{file.FullName}\t{DateTime.Now}");
-                    ftp.Upload($"{file.FullName}", remotePath + file.Name);
-                });
-            }
+            //foreach (var file in files)
+            //{
+            //    Task.Factory.StartNew(()=>
+            //    {
+            //        Console.WriteLine($"Копирование:\t{file.FullName}\t{DateTime.Now}");
+            //        ftp.Upload($"{file.FullName}", remotePath + file.Name);
+            //    });
+            //}
+            //foreach (var dir in dirs)
+            //{
+
+            //}
             Console.ReadKey();
         }        
     }
