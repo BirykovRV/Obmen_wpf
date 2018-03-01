8ef31fe9dfdfc35c4ff42fc98cb7416af06839f6
Roman Birukov <r.v.birukov@yandex.ru>
2018-03-01 16:22:31 +0300

    логирование сбоев

--- Obmen_wpf/Model/CopyEspp.cs
+++ Obmen_wpf/Model/CopyEspp.cs
@@ -1,10 +1,4 @@
 ﻿using Obmen_wpf.Properties;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Obmen_wpf.Model
 {
