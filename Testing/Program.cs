40d76aa832d0d8c5f80dcca1b6e0e7826648c2b2
Roman Birukov <r.v.birukov@yandex.ru>
2018-03-29 11:37:26 +0300

    init

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -12,34 +12,46 @@ namespace Testing
 {
     class Program
     {
+        private static Object syncObject = new Object();
+        private static void Write()
+        {
+            lock (syncObject)
+            {
+                Console.WriteLine("test");
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Press any key to start");
 
             Console.ReadKey();
 
-            StartUpload(@"G:\Реестр коммунальных платежей", "200001/Реестр коммунальных платежей/");
+            lock (syncObject)
+            {
+                Write();
+            }
+            //StartUpload(@"G:\Реестр коммунальных платежей", "200001/Реестр коммунальных платежей/");
 
             Console.ReadKey();
         }
-        private static void StartUpload(string pathFrom, string pathTo)
-        {
-            ftp server = new ftp("10.87.6.143", "support", "trd19afo");
+        //private static void StartUpload(string pathFrom, string pathTo)
+        //{
+        //    ftp server = new ftp("10.87.6.143", "support", "trd19afo");
 
-            string[] files = Directory.GetFiles(pathFrom, "*.*");
-            string[] subDirs = Directory.GetDirectories(pathFrom);
+        //    string[] files = Directory.GetFiles(pathFrom, "*.*");
+        //    string[] subDirs = Directory.GetDirectories(pathFrom);
 
-            foreach (string file in files)
-            {
-                server.Upload(pathTo + Path.GetFileName(file), file);
-            }
+        //    foreach (string file in files)
+        //    {
+        //        server.Upload(pathTo + Path.GetFileName(file), file);
+        //    }
 
-            foreach (string subDir in subDirs)
-            {
-                server.CreateDirectory(pathTo + Path.GetFileName(subDir));
-                StartUpload(subDir, pathTo + Path.GetFileName(subDir) + "/");
-            }
-        }
+        //    foreach (string subDir in subDirs)
+        //    {
+        //        server.CreateDirectory(pathTo + Path.GetFileName(subDir));
+        //        StartUpload(subDir, pathTo + Path.GetFileName(subDir) + "/");
+        //    }
+        //}
     }
 }
-
